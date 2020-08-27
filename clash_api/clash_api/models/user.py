81ec7350a7b3c18bef7bb53f71cbd19029f9abd9
strangiato <trevorroyer@gmail.com>
2020-08-26 21:35:45 -0500

    testing forced fix

--- clash_api/clash_api/models/user.py
+++ clash_api/clash_api/models/user.py
@@ -2,7 +2,8 @@ from clash_api.extensions import db, pwd_context
 
 
 class User(db.Model):
-    """Basic user model"""
+    """
+    Basic user model"""
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
