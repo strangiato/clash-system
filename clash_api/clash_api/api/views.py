c42691988d3f8615a8bfaf2f7bc2c78dd7b53a10
strangiato <trevorroyer@gmail.com>
2020-09-07 16:21:05 -0500

    add player views

--- clash_api/clash_api/api/views.py
+++ clash_api/clash_api/api/views.py
@@ -6,6 +6,8 @@ from clash_api.api.resources import UserResource, UserList
 from clash_api.api.schemas import UserSchema
 from clash_api.api.resources import ClanResource, ClanList
 from clash_api.api.schemas import ClanSchema
+from clash_api.api.resources import PlayerResource, PlayerList
+from clash_api.api.schemas import PlayerSchema
 
 blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
 api = Api(blueprint)
@@ -17,6 +19,9 @@ api.add_resource(UserList, "/users", endpoint="users")
 api.add_resource(ClanResource, "/clans/<int:clan_id>", endpoint="clan_by_id")
 api.add_resource(ClanList, "/clans", endpoint="clans")
 
+api.add_resource(PlayerResource, "/players/<int:player_id>", endpoint="player_by_id")
+api.add_resource(PlayerList, "/players", endpoint="players")
+
 
 @blueprint.before_app_first_request
 def register_views():
@@ -27,6 +32,10 @@ def register_views():
     apispec.spec.components.schema("ClanSchema", schema=ClanSchema)
     apispec.spec.path(view=ClanResource, app=current_app)
     apispec.spec.path(view=ClanList, app=current_app)
+
+    apispec.spec.components.schema("PlayerSchema", schema=PlayerSchema)
+    apispec.spec.path(view=PlayerResource, app=current_app)
+    apispec.spec.path(view=PlayerList, app=current_app)
 
 
 @blueprint.errorhandler(ValidationError)
