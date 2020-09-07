2a0891762ecbba8184f8f50340d6e7042b5256da
strangiato <trevorroyer@gmail.com>
2020-09-07 16:03:31 -0500

    update models init

--- clash_api/clash_api/models/__init__.py
+++ clash_api/clash_api/models/__init__.py
@@ -1,6 +1,7 @@
 from clash_api.models.user import User
 from clash_api.models.blacklist import TokenBlacklist
 from clash_api.models.clan import Clan
+from clash_api.models.player import Player
 
 
-__all__ = ["User", "TokenBlacklist", "Clan"]
+__all__ = ["User", "TokenBlacklist", "Clan", "Player"]
